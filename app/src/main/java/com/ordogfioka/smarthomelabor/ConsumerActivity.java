18eab53bef538eaaf2144141018354ed889711ff
ordogfioka <megyesi.attika@gmail.com>
2016-11-26 21:58:44 +0100

    comminication with google

---
+++ app/src/main/java/com/ordogfioka/smarthomelabor/ConsumerActivity.java
@@ -0,0 +1,74 @@
+package com.ordogfioka.smarthomelabor;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+
+public class ConsumerActivity extends AppCompatActivity {
+    ListView listView = null;
+    List<String> messages = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumer);
+        listView = (ListView)findViewById(R.id.consumerListView);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,messages);
+        listView.setAdapter(adapter);
+        messages.add((String) getIntent().getExtras().get("topic"));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new DoBackgroundTask().execute();
+        System.out.println("hello");
+    }
+
+    private class DoBackgroundTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response = "";
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpPost = new HttpGet("http://google.com");
+
+            try {
+                // Execute POST
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                response = httpResponse.toString();
+            } catch (Exception e) {
+                response = e.toString();
+            }
+            final String finalResponse = response;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter.add(finalResponse);
+                }
+            });
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    new DoBackgroundTask().execute();
+                }
+            }, 1000);
+        }
+    }
+}
--- app/src/main/java/com/ordogfioka/smarthomelabor/MainActivity.java
+++ app/src/main/java/com/ordogfioka/smarthomelabor/MainActivity.java
@@ -1,43 +1,81 @@
 package com.ordogfioka.smarthomelabor;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.client.HttpClient;
-import cz.msebera.android.httpclient.client.methods.HttpGet;
-import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RunnableFuture;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
+    ListView listView = null;
+    List<String> arrayOfUsers = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    TextView textView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv =(TextView)findViewById(R.id.textBox);
+        listView =(ListView)findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayOfUsers);
+        listView.setAdapter(adapter);
+        textView = (TextView) findViewById(R.id.textView);
+        tmp();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedFromList =(String) (listView.getItemAtPosition(i));
+                Intent intent = new Intent(MainActivity.this,ConsumerActivity.class);
+                intent.putExtra("topic",selectedFromList);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void tmp() {
+        arrayOfUsers.add("Alma");
+        arrayOfUsers.add("Barack");
+        arrayOfUsers.add("Körte");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
     }
 
     public void onClick(View view) {
         new Thread(new Runnable() {
             public void run() {
-                try {
-                    HttpClient client = new DefaultHttpClient();
-                    String postURL = "http://google.com";
-                    HttpGet post = new HttpGet(postURL);
+                OkHttpClient client = new OkHttpClient();
 
-                    HttpResponse responseGet = client.execute(post);
-                    tv.setText(responseGet.toString());
-                } catch (Exception e) {
+                Request request = new Request.Builder()
+                        .url("http://google.com")
+                        .build();
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
+                final Response finalResponse = response;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(finalResponse.toString());
+                    }
+                });
+
             }
         }).start();
     }
